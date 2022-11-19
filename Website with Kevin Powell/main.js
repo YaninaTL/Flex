f68ee13057731d1e5759527e2a09102e9c7c6146
Yanina <moontmorency@gmail.com>
2022-11-19 20:49:25 +0300

    change js method

--- Website with Kevin Powell/main.js
+++ Website with Kevin Powell/main.js
@@ -1,6 +1,12 @@
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
 
+// navToggle.addEventListener("click", () => {
+//   primaryNav.classList.toggle("opened");
+// });
+// Toggling a class
+
 navToggle.addEventListener("click", () => {
-  primaryNav.classList.toggle("opened");
+  primaryNav.toggleAttribute("data-visible");
 });
+// Toggling an attribute
