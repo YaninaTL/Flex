b777688c519959c6b53c3f012223ee6ebb7d9295
Yanina <moontmorency@gmail.com>
2022-11-19 21:08:24 +0300

    style mob nav links

--- Website with Kevin Powell/main.js
+++ Website with Kevin Powell/main.js
@@ -7,6 +7,14 @@ const primaryNav = document.querySelector(".primary-navigation");
 // Toggling a class
 
 navToggle.addEventListener("click", () => {
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
   primaryNav.toggleAttribute("data-visible");
 });
 // Toggling an attribute
+
+// to check
+// primaryNav.hasAttribute("data-visible")
+//   ? console.log("true")
+//   : console.log("false");
