1dc33fd144c934f0215dbb51016a260c286b688f
Yanina Tormos Lopez <moontmorency@gmail.com>
2023-10-11 23:03:20 +0200

    js dynamic toggle and date

--- RSS library stage 0/app.js
+++ RSS library stage 0/app.js
@@ -1,1 +1,47 @@
 // toggle
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+/*navToggle.addEventListener("click", function () {
+  linksContainer.classList.toggle("show-links");
+});*/
+
+// KNOWN height, so, it's added to css and this works in js
+// if we add one more link -> doesn't work, so, it's not DYNAMIC
+
+navToggle.addEventListener("click", function () {
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  //height = 0 by default as we hide it
+  const linksHeight = links.getBoundingClientRect().height;
+
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
+// height AUTO to links-container in css!
+//height = 150 as we have 3 links, so, if we change the quantity the height will be autocalculated
+
+// fixed navbar
+
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.scrollY;
+  const navHeight = document
+    .querySelector(".navbar")
+    .getBoundingClientRect().height;
+
+  if (scrollHeight > navHeight) {
+    document.querySelector(".navbar").classList.add("fixed-nav");
+  } else {
+    document.querySelector(".navbar").classList.remove("fixed-nav");
+  }
+});
+
+//date
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
