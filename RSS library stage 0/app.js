e8391e5f9bfd8ba966c0948b33beb52fa00a3099
Yanina Tormos Lopez <moontmorency@gmail.com>
2023-10-11 22:52:20 +0200

    set up initial css for nav

--- RSS library stage 0/app.js
+++ RSS library stage 0/app.js
@@ -1,12 +1,1 @@
-// hamburger menu
-
-document.addEventListener("DOMContentLoaded", function () {
-  const hamburgerMenu = document.querySelector(".hamburger-menu");
-
-  const nav = document.querySelector(".nav-menu ul");
-
-  // toggle
-  hamburgerMenu.addEventListener("click", function () {
-    nav.classList.toggle("active");
-  });
-});
+// toggle
