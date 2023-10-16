98717f5c41ea6c607f9d1a4d757a51babffff30b
Yanina Tormos Lopez <moontmorency@gmail.com>
2023-10-16 21:39:45 +0200

    fix profile icon & add functionality

--- RSS library stage 0/app.js
+++ RSS library stage 0/app.js
@@ -45,3 +45,31 @@ window.addEventListener("scroll", function () {
 //date
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
+
+// profile
+document.addEventListener("DOMContentLoaded", function () {
+  const profileButton = document.getElementById("profile-button");
+  const menu = document.querySelector(".menu");
+
+  profileButton.addEventListener("click", function (event) {
+    event.stopPropagation(); // Prevent the click event from reaching the body
+
+    if (menu.style.visibility === "hidden" || menu.style.visibility === "") {
+      menu.style.visibility = "visible";
+      menu.style.opacity = "1";
+    } else {
+      menu.style.visibility = "hidden";
+      menu.style.opacity = "0";
+    }
+  });
+
+  // Close the menu when clicking outside of it
+  document.body.addEventListener("click", function () {
+    menu.style.visibility = "hidden";
+    menu.style.opacity = "0";
+  });
+
+  menu.addEventListener("click", function (event) {
+    event.stopPropagation(); // Prevent the click event from closing the menu
+  });
+});
